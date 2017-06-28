bd388f7e1bc3ba5ee87584bbd7a2eac06884b1a3
Jay Kelly <comital333@gmail.com>
2017-06-29 09:29:20 +1000

    add text to main image

--- client/src/Commercial.js
+++ client/src/Commercial.js
@@ -3,7 +3,12 @@ import image from './images/solar_commercial_cropped.jpg'
 
 const Commercial = () => {
   return (
-    <div><img src={image} alt='commercial'/></div>
+    <div className='commercialContainer'>
+      <div className='mainImage'>
+        <img src={image} alt='commercial'/>
+        <h1>Allow us to tailor a solar solution that is right for your business.</h1>
+      </div>
+    </div>
   )
 
 }
