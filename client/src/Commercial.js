3e7fd273557802c7210cf3b724bf8d1d14e019c2
Jay Kelly <comital333@gmail.com>
2017-06-29 18:57:41 +1000

    added copy

--- client/src/Commercial.js
+++ client/src/Commercial.js
@@ -8,6 +8,13 @@ const Commercial = () => {
         <img src={image} alt='commercial'/>
         <h1>Allow us to tailor a solar solution that is right for your business.</h1>
       </div>
+      <div className='mainContent'>
+        <div className='services'>
+          <h1>Why solar power your business with solar?</h1>
+          <p>Solar power is an affordable way to lower your electricity costs by taking control of your energy needs. Solar
+          power can protect your company from rising energy expenses and may also promote the sustainable values of your business</p>
+        </div>
+      </div>
     </div>
   )
 
