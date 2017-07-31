f3771f3bd46ab7e08c3f9b5d86e248a4234b7c0b
Jay Kelly <comital333@gmail.com>
2017-07-31 10:07:52 +1000

    added new content block

--- client/src/Commercial.js
+++ client/src/Commercial.js
@@ -2,6 +2,7 @@ import React from 'react'
 import image from './images/solar_commercial_cropped.jpg'
 import image1 from './images/barley-field.jpg'
 import image2 from './images/solar-panel-2.png'
+import image3 from './images/solar-install.jpg'
 
 const Commercial = () => {
   return (
@@ -23,19 +24,27 @@ const Commercial = () => {
             <img src={image1}/>
           </div>
           <div className='contentBlock'>
-            <h1>Break free from rising energy prices</h1>
-            <p>Energy prices are constantly on the rise. Solar power can significantly reduce monthly electricity bills
-              by taking advantage of our most abundent and free energy source.</p>
+            <h1>Go Green</h1>
+            <p>Reduce the carbon footprint with solar and build a clean image for your business</p>
           </div>
         </div>
         <div className='block'>
           <div className='contentBlock'>
-            <h1>The right solution for your roof</h1>
-            <p>Freedom Energy offers cost-effective and flexible solutions to maxamize solar performance.
-              </p>
+            <h1>Secure your business for the future</h1>
+            <p>Rising energy prices can put a strain on any business. Solar power is a cost-effective solution that can lead to massive savings in the future.
+            </p>
           </div>
           <div className='imageBlock'>
             <img src={image2}/>
+          </div>
+        </div>
+        <div className='block'>
+          <div className='imageBlock'>
+            <img src={image3}/>
+          </div>
+          <div className='contentBlock'>
+            <h1>Get the solution that is right for your business</h1>
+            <p>Every business is different and will require a different solution. At Freedom Energy we talior a solution that is right for you at the lowest cost.</p>
           </div>
         </div>
       </div>
