f6ace752c016d5bca7ef9bacce16db6c7ada27ec
Jay Kelly <comital333@gmail.com>
2017-08-03 12:36:03 +1000

    handle post request

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 const generatePassword = require('password-generator');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -28,7 +32,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+app.post('/contactForm', (req, res) => {
+  const info = req.body.info
+  res.send(info)
+})
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Freedom Energy is listen on port ${port}`);
