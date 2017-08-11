1526ab1bfdddb9519068ca0e06131be0fe6548bb
Jay Kelly <comital333@gmail.com>
2017-08-11 12:03:05 +1000

    new component - SmallMenu

---
+++ client/src/SmallMenu.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import { Menu as SemanticMenu, Dropdown, Icon, Segment} from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
+
+const SmallMenu = () => {
+  return (
+    <div className='menu'>
+      <div className='left-menu'>
+        <NavLink to="/"><img src="logo2.png" alt="Freedom Energy" className="logo"/></NavLink>
+      </div>
+      <div className='smallMenu'>
+      <SemanticMenu attached='top'>
+        <Dropdown item icon='content' simple>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <NavLink to="/residential" className="item">Residential</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <NavLink to="/commercial" className="item">Commercial</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <NavLink to="/utilities" className="item">Utilities</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <NavLink to="/contact" className="item">Contact</NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <a href='tel:+1300695644' title="Call Freedom Solar" className="item call">1300 695 644</a>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </SemanticMenu>
+    </div>
+    </div>
+  )
+}
+
+export default SmallMenu
