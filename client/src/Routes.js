9b6434c32cf409c9ef618d86f9b945e8d977152a
Jay Kelly <comital333@gmail.com>
2017-09-28 19:55:31 +1000

    add roofleasing

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import Home from './Home'
 import Residential from './Residential'
 import Commercial from './Commercial'
+import RoofLeasing from './RoofLeasing'
 import Utilities from './Utilities'
 import Contact from './Contact'
 import createHistory from 'history/createBrowserHistory'
@@ -26,6 +27,7 @@ const Routes = (props) => {
       <Route exact path="/" render={homeComponent} history={history}/>
       <Route path="/residential" component={Residential} history={history}/>
       <Route path="/commercial" component={Commercial} history={history}/>
+      <Route path="/roofleasing" component={RoofLeasing} history={history}/>
       <Route path="/utilities" component={Utilities} history={history}/>
       <Route path="/Contact" component={Contact} history={history}/>
     </div>
