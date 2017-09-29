ddef8d6d6b76b64fe5ff5c5000313c4e019c5a31
Jay Kelly <comital333@gmail.com>
2017-09-29 10:34:47 +1000

    use content blocks instead of text

--- client/src/RoofLeasing.js
+++ client/src/RoofLeasing.js
@@ -3,9 +3,8 @@ import { Button, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import SubMenuCommercial from './SubMenuCommercial'
 import image from './images/solar_building.jpg'
-import image1 from './images/barley-field.jpg'
-import image2 from './images/solar-panel-2.png'
-import image3 from './images/solar-install.jpg'
+import image1 from './images/solar-panel-sky.jpg'
+import image2 from './images/solar-install.jpg'
 import amanda from './images/amanda.jpg'
 import tick from './images/tick2.png'
 
@@ -26,22 +25,34 @@ const RoofLeasing = () => {
           Freedom will own and operate the solar power equipment and meet all costs associated
           with construction, operation and maintenance.</p>
         </div>
-        <div className='services'>
-          <h1>ROOF LEASE AGREEMENT</h1>
-          <p>Freedom Energy rents the roof and farms the sunshine.
-          The ownership, cost of design, installation and maintenance of the solar system is covered
-          by Freedom Energy for the term of the agreement.
-          Freedom Energy will on sell the energy to energy retailers through existing PPA’s , off take agreement.
-          Alternatively tenants have the option to purchase the electricity produced at discounted grid
-          electricity rates.</p>
+      </div>
+      <div className='contentBlocks roofleasing'>
+        <div className='block'>
+          <div className='imageBlock imageBlock1'>
+            <img src={image1}/>
+          </div>
+          <div className='contentBlock contentBlock1'>
+            <h1>Roof lease agreement</h1>
+            <p>Freedom Energy rents the roof and farms the sunshine.
+            The ownership, cost of design, installation and maintenance of the solar system is covered
+            by Freedom Energy for the term of the agreement.
+            Freedom Energy will on sell the energy to energy retailers through existing PPA’s , off take agreement.
+            Alternatively tenants have the option to purchase the electricity produced at discounted grid
+            electricity rates.</p>
+          </div>
         </div>
-        <div className='services'>
-          <h1>BENEFITS FOR PROPERTY OWNERS</h1>
-          <p>This arrangement particularly suits property owners who have one main meter (NMI) with an electricity
-           supply agreement and sub meters for tenants who purchase their electricity from the property owner.
-           Solar electricity consumed at the main meter is divided equitably to the sub meters in the same way as
-           the normal electricity consumption and can be charged to the sub meter at the retail electricity price.
-           </p>
+        <div className='block'>
+          <div className='contentBlock contentBlock2'>
+            <h1>Benifits for property owners</h1>
+            <p>This arrangement particularly suits property owners who have one main meter (NMI) with an electricity
+             supply agreement and sub meters for tenants who purchase their electricity from the property owner.
+             Solar electricity consumed at the main meter is divided equitably to the sub meters in the same way as
+             the normal electricity consumption and can be charged to the sub meter at the retail electricity price.
+             </p>
+          </div>
+          <div className='imageBlock imageBlock2'>
+            <img src={image2}/>
+          </div>
         </div>
       </div>
       <div className='services'>
